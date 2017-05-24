0558fe63941605790c00d708e4d16382952a0763
tdimnet <thomas.dimnet@gmail.com>
2017-05-24 17:07:07 +0200

    Refactore the app

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Foo
+        </Text>
+      </View>
+    );
+  }
+} // End: App
+
+export default App;
